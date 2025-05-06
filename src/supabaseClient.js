2b77b453f6cda8f67b3a43f4a99cfbc2731c76e3
ManuWa <manuelwarrior000@gmail.com>
2025-05-06 07:42:26 +0200

    Pruebas fallos 1

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
+console.log("Supabase URL:", supabaseUrl);  // Esto debería imprimir la URL de Supabase
+console.log("Supabase Key:", supabaseKey);  // Esto debería imprimir la clave de Supabase
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
