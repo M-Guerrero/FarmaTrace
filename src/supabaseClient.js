4016260a6612a862f1e4e91242cd1af89400945f
ManuWa <manuelwarrior000@gmail.com>
2025-05-06 08:02:51 +0200

    Cambiando KEY prueba 2

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
-
-console.log("Supabase URL:", supabaseUrl);  // Esto debería imprimir la URL de Supabase
-console.log("Supabase Key:", supabaseKey);  // Esto debería imprimir la clave de Supabase
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
